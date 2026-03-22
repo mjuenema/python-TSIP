6f1955f5fb0047410df0e5c1f679c77fc3aa557f
Markus Juenemann <markus@juenemann.net>
2026-03-22 22:54:36 +1100

    Updated VERSION in tsip/__init__.py

--- tsip/__init__.py
+++ tsip/__init__.py
@@ -6,7 +6,7 @@ from tsip.llapi import *
 from tsip.hlapi import *
 
 NAME = 'tsip'
-VERSION = '0.4.1'
+VERSION = '0.4.2'
 LICENSE = 'BSD License'
 AUTHOR = 'Markus Juenemann'
 EMAIL = 'markus@juenemann.net'
